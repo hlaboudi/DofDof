6952d4a919e2eb31224b7eeecdd56b5e063fb352
hakim <laboudi.hakim@gmail.com>
2019-03-26 10:47:04 +0100

    IRecycler View OK !

--- app/src/main/java/com/example/dofdof/RestDofusApi.java
+++ app/src/main/java/com/example/dofdof/RestDofusApi.java
@@ -1,5 +1,10 @@
 package com.example.dofdof;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RestDofusApi {
     @GET("famillier")
+    Call<RestDofusResponse> getListDofus();
+
 }
